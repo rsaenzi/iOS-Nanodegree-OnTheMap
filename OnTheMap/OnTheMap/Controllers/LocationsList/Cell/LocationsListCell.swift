bc1aca7dcd1d047e038f1fdc55b28c75c334bcda
Rigoberto Saenz Imbacuan <beto456789@gmail.com>
2017-12-24 22:36:54 -0500

    UI Navigation added via storyboards

---
+++ OnTheMap/OnTheMap/Controllers/LocationsList/Cell/LocationsListCell.swift
@@ -0,0 +1,13 @@
+//
+//  LocationsListCell.swift
+//  OnTheMap
+//
+//  Created by Rigoberto Sáenz Imbacuán on 12/24/17.
+//  Copyright © 2017 Rigoberto Sáenz Imbacuán. All rights reserved.
+//
+
+import UIKit
+
+class LocationsListCell: UITableViewCell {
+    
+}
